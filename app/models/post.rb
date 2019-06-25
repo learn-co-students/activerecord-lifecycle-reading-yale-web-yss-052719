c6bd175cb678a40759e4cec9011d5285e670b085
github username <github email address>
2019-06-25 13:30:19 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,21 @@
 class Post < ActiveRecord::Base
+ 
+belongs_to :author
+validate :is_title_case 
 
-  belongs_to :author
-  validate :is_title_case 
+before_validation :make_title_case 
 
-  private
+# New Code!!
 
-  def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
-    end
+private
+
+def is_title_case
+  if title.split.any?{|w|w[0].upcase != w[0]}
+    errors.add(:title, "Title must be in title case")
   end
+end
 
-  def make_title_case
-    self.title = self.title.titlecase
-  end
+def make_title_case
+  self.title = self.title.titlecase
+end
 end
